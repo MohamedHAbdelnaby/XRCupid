b9b39f8d1585f27c02c2f7bde81894af39c5bec1
Lucas Martinic <42948357+lucas-martinic@users.noreply.github.com>
2025-01-24 14:40:25 -0500

    Imported avatar

--- Assets/Scripts/DateManager.cs
+++ Assets/Scripts/DateManager.cs
@@ -1,4 +1,6 @@
+using Convai.Scripts.Runtime.UI;
 using Meta.WitAi.Dictation;
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +8,18 @@ using UnityEngine;
 public class DateManager : MonoBehaviour
 {
     [SerializeField] private DictationService _dictation;
+    [SerializeField] private ConvaiChatUIHandler convai;
+    [SerializeField] string testPrompt = "Hello, how are you?";
 
     // Start is called before the first frame update
     void Start()
     {
         _dictation.Activate();
+    }
+
+    [Button]
+    private void TestPrompt()
+    {
+        convai.SendPlayerText(testPrompt);
     }
 }
